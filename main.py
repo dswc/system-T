82372dac68af7990dca54a582500eca190740ace
dswc <multi.summer@gmail.com>
2025-03-19 11:40:24 +0800

    interactive with user

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from Teacher import Teacher
 
-teacher1 = Teacher('王老師')
-teacher2 = Teacher('朱老師')
+teacher_name_list = ['王老師', '朱老師', '陳老師']
+teacher_list = []
+for i in range(len(teacher_name_list)):
+    teacher = Teacher(teacher_name_list[i])
+    teacher_list.append(teacher)
+print(teacher_list)
 
-teacher1.getName()
-teacher2.getName()
+print('====================')
+for i in range(len(teacher_name_list)):
+    print(str(i), ':', teacher_name_list[i])
+print('====================')
+
+
+print('[Message] End')
